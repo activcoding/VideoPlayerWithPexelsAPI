ae493e1ecc60a8efd1f61c6f41e45337b31564b8
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-04-07 10:19:16 +0200

    Initial Commit

---
+++ VideoPlayerWithPexelsAPI/VideoPlayerWithPexelsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerWithPexelsAPIApp.swift
+//  VideoPlayerWithPexelsAPI
+//
+//  Created by Tommy Ludwig on 07.04.23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerWithPexelsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
